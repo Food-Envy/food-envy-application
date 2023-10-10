bc21767178eece1a27ad140e4e18f0e6608ddc7a
Aydan Smith <aydansmith@Aydans-MacBook-Pro.local>
2023-10-09 20:27:34 -0500

    initial app bar added

--- food_envy_application/lib/auth_gate.dart
+++ food_envy_application/lib/auth_gate.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
-import 'main.dart';
+import 'home_page.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
